2dfdaa56573a291694c3602e314151493ccf0220
Lazarte Mansicidor Alexis Saul <Alexis_lazarte@hotmail.com>
2025-06-02 00:02:39 -0300

    Registro de usuarios

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from src.services.clear_console import clear_console
 from src.services.ver_dispositivos import ver_dispositivos
 from src.services.eliminar_dispositivo import eliminar_dispositivo
 from src.services.automatizaciones import automatizaciones
+from src.services.usuarios import crear_usuario
+from src.services.usuarios import iniciar_sesion
 
 dispositivos = []
 
@@ -11,40 +13,41 @@ while True:
     clear_console()
     menu = int(
         input(
-            '\nBienvenidos al Sistema SmartHome Solutions\n\nA continuación, elija una opción:\n\n1) - Gestionar los dispositivos\n\n2) - Gestionar automatizaciones\n\n\n' \
+            '\nBienvenidos al Sistema SmartHome Solutions\n\nA continuación, elija una opción:\n\n1) - Iniciar Sesión\n\n2) - Registrarse\n\n\n' \
             '0) - Salir\n\nOpción elegida: '
         ))
     if menu == 1:
         while True:
             clear_console()
-            menu_dispositivos = int(
-                input(
-                    '\nGestión de dispositivos\n\n1) - Agregar nuevo dispositivo\n\n2) - Ver todos los dispositivos disponibles\n\n3) - Buscar dispositivos\n\n4) - Eliminar dispositivos\n\n\n0) - Volver\n\nOpción elegida: '
-                ))
+            iniciar_sesion()
 
-            if menu_dispositivos == 1:
-                agregar_dispositivo(dispositivos)
+            # menu_usuarios = int(
+            #     input(
+            #         '\nGestión de dispositivos\n\n1) - Agregar nuevo dispositivo\n\n2) - Ver todos los dispositivos disponibles\n\n3) - Buscar dispositivos\n\n4) - Eliminar dispositivos\n\n\n0) - Volver\n\nOpción elegida: '
+            #     ))
 
-            elif menu_dispositivos == 2:
-                ver_dispositivos(dispositivos,
-                                 mostrarInput=True,
-                                 noEsenciales=False)
+            # if menu_usuarios == 1:
+            #     agregar_dispositivo(dispositivos)
 
-            elif menu_dispositivos == 3:
-                buscar_dispositivo(dispositivos)
+            # elif menu_usuarios == 2:
+            #     ver_dispositivos(dispositivos,
+            #                      mostrarInput=True,
+            #                      noEsenciales=False)
 
-            elif menu_dispositivos == 4:
-                eliminar_dispositivo(dispositivos)
+            # elif menu_usuarios == 3:
+            #     buscar_dispositivo(dispositivos)
 
-            else:
-                break
+            # elif menu_usuarios == 4:
+            #     eliminar_dispositivo(dispositivos)
+
+            # else:
+            #     break
 
     elif menu == 2:
-        automatizaciones(dispositivos)
+        crear_usuario()
+        # automatizaciones(dispositivos)
 
     else:
         clear_console()
         print('\n¡Muchas gracias por utilizar nuestros servicios.!\n')
         break
-
-#Comentario de prueba Lazarte Alexis
---
+++ src/services/usuarios.py
@@ -0,0 +1,49 @@
+from src.services.clear_console import clear_console
+
+usuarios =[]
+
+def crear_usuario():
+    clear_console()
+    usuario = {}
+    usuario['nombre'] = input('\nIngrese nombre de nuevo usuario: ').lower()
+    clear_console()
+    usuario['apellido'] = input('\nIngrese su apellido: ').lower()
+    clear_console()
+    usuario['email'] = input('\nIngrese su email: ').lower()
+    usuario['rol'] = ''
+    clear_console()
+    usuarios.append(usuario)
+    clear_console()
+    print(usuarios)
+    input(
+        f'\nUsuario creado con exito!\n\nPresione ENTER para continuar '
+    )
+
+def iniciar_sesion():
+    clear_console()
+    usuario_registrado = {}
+    usuario_registrado['nombre'] = input('\nIniciar sesion\n\nIngrese nombre de usuario: ').lower()
+    usuario_registrado['email'] = input('\nIngrese email: ').lower()
+
+    for usuario in usuarios:
+        if usuario_registrado['nombre'] == usuario['nombre'].lower() and usuario_registrado['email'] == usuario['email'].lower():
+            clear_console()
+            while True:
+                menu_usuario = int(input('\n1) - Consultar datos personales\n\n2) - Ejecutar automatizaciones\n\n3) - Consultar dispositivos\n\n\n0) - Volver\n\nOpcion elegida: '))
+
+                if menu_usuario == 1:
+                    clear_console()
+                    print(usuario)
+                    input('\n\nPresione ENTER para continuar ')
+                
+                elif menu_usuario == 2:
+                    clear_console()
+                    print()
+                    input()
+
+
+
+
+        else:
+            print("usuario no regristrado")
+            input()
